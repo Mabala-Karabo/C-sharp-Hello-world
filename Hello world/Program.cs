666076280c8786f399c77f173c1625ded589305a
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-11 15:14:07 +0200

    Add project files.

---
+++ Hello world/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Welcome to C#");
+            Console.ReadLine(); //this is a comment in C#
+        }
+    }
+}
